f29ab4f7852eaeac34b7533091bdb24123d0e12d
SopuluNwakaeze <sopnwakaeze@gmail.com>
2024-11-13 13:42:35 -0700

    The beginning of a Calculator project
    I have created files to begin building the calculator and I added
    4 functions that the calculator will use

---
+++ index-script.js
@@ -0,0 +1,21 @@
+
+function add(x,y){
+    return x + y;
+}
+
+function subtract(x,y){
+    return x - y;
+}
+
+function multiply(x,y){
+    return x * y;
+}
+
+function divide(x,y){
+    return x / y;
+}
+
+console.log(add(2,4))
+console.log(subtract(2,4))
+console.log(multiply(2,4))
+console.log(divide(2,4))
