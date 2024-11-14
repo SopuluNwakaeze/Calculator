35cd8304766ae0af16e1a95da82d4b49363f7826
SopuluNwakaeze <sopnwakaeze@gmail.com>
2024-11-14 14:24:11 -0700

    Added initial functionality to the clear button and also changed the styling of the Calculator
    I have added the initial functionality to the clear button, it will reset
    the screen to 0 when pressed
    I updated the stying to give it more of a traditional calculator feel.

--- index-script.js
+++ index-script.js
@@ -1,6 +1,13 @@
 var fOperand;
 var sOperand;
 var operator;
+var length;
+const numPadButtons = document.querySelectorAll(".numpad-button")
+const clearButton = document.querySelector(".clear-button")
+const display = document.querySelector(".screen-content")
+const buffer1 = [];
+const buffer2 = [];
+
 
 function add(x,y){
     return x + y;
@@ -37,3 +44,27 @@ function operate(operator, x, y){
             break;     
     }
 }
+
+numPadButtons.forEach((button) => button.addEventListener("click",function(e){  
+    buffer1.push(button.textContent);
+    display.textContent = buffer1.toString();
+    console.log("length on button press:" + buffer1.length);
+}))
+
+clearButton.addEventListener("click", function(e){
+    length = buffer1.length;
+    buffer1.push(0)
+    console.log("length after clear press:" + buffer1.length)
+    for (var i = 0; i < length; i++){
+        console.log(buffer1.shift());  
+        console.log("length after" + i + "shift" + buffer1.length)
+
+    }
+    
+    console.log(buffer1.toString());
+    // buffer1.push(0);
+    display.textContent = buffer1.toString();
+})
+
+
+
