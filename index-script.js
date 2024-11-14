6a888d828a6035bd395e586bf61bbe2ed4956a09
SopuluNwakaeze <sopnwakaeze@gmail.com>
2024-11-14 15:52:04 -0700

    Added functionality for basic use of the calc
    The calculator is able to take a first operand
    and operator and a second operand and solve it

--- index-script.js
+++ index-script.js
@@ -1,10 +1,15 @@
 var fOperand;
 var sOperand;
-var operator;
+var operator = "";
 var length;
-const numPadButtons = document.querySelectorAll(".numpad-button")
-const clearButton = document.querySelector(".clear-button")
-const display = document.querySelector(".screen-content")
+var length2;
+const numPadButtons = document.querySelectorAll(".numpad-button");
+const clearButton = document.querySelector(".clear-button");
+const display = document.querySelector(".screen-content");
+const numbersButtons = document.querySelectorAll("#number");
+const operatorButtons = document.querySelectorAll("#operator");
+const equalsButton = document.querySelector("#equals");
+const decimalButton = document.querySelector("#decimal");
 const buffer1 = [];
 const buffer2 = [];
 
@@ -14,6 +19,10 @@ function add(x,y){
 }
 
 function subtract(x,y){
+    console.log("hello")
+    console.log(x)
+    console.log(y)
+
     return x - y;
 }
 
@@ -28,42 +37,86 @@ function divide(x,y){
 function operate(operator, x, y){
     switch (operator){
         case "+":
-            add(x,y);
-            break;
+            return add(x,y);
 
         case "-":
-            subtract(x,y);
-            break;
+            return subtract(x,y);
 
         case "x":
-            multiply(x,y);
-            break;
+            return multiply(x,y);
 
         case "/":
-            divide(x,y);
-            break;     
+            return divide(x,y);    
     }
 }
+console.log(operate("-",2,3))
 
-numPadButtons.forEach((button) => button.addEventListener("click",function(e){  
-    buffer1.push(button.textContent);
-    display.textContent = buffer1.toString();
-    console.log("length on button press:" + buffer1.length);
+equalsButton.addEventListener("click", function(e){
+    length = buffer1.length;
+    length2 = buffer2.length;
+    fOperand = Number(buffer1.toString().replace(/,/g,""));
+    sOperand = Number(buffer2.toString().replace(/,/g,""));
+    console.log(fOperand)
+    console.log(sOperand)
+    console.log(operator)
+
+
+    for (var i = 0; i < length; i++){
+        buffer1.shift();  
+    }
+    console.log(operate(operator,fOperand,sOperand))
+    buffer1.push(operate(operator,fOperand,sOperand));
+    for (var i = 0; i < length2; i++){
+        buffer2.shift();  
+    }
+        display.textContent = buffer1.toString();
+        console.log("hello")
+})
+
+operatorButtons.forEach((button) => button.addEventListener("click", function(e){
+    operator = button.textContent;
 }))
+
+numbersButtons.forEach((button) => button.addEventListener("click", function(e){
+    if(operator === ""){
+        if(Number(buffer1[0]) === 0){
+            buffer1.pop();
+        }
+        if(buffer1.length >= 9){
+            console.log("full");
+        }
+        else{
+            buffer1.push(button.textContent);
+        }
+        display.textContent = buffer1.toString().replace(/,/g,"");
+        console.log(buffer1);
+    }
+    // display.textContent = buffer1.toString().replace(/,/,"");
+
+    if(operator !== ""){
+        if(buffer2.length >= 9){
+            console.log("full");
+        }
+        else{
+            buffer2.push(button.textContent);
+        }
+        display.textContent = Number(buffer2.toString().replace(/,/g,""));
+    }
+}))
+
+// numPadButtons.forEach((button) => button.addEventListener("click",function(e){  
+//     buffer1.push(button.textContent);
+//     display.textContent = buffer1.toString();
+
+// }))
 
 clearButton.addEventListener("click", function(e){
     length = buffer1.length;
     buffer1.push(0)
-    console.log("length after clear press:" + buffer1.length)
     for (var i = 0; i < length; i++){
-        console.log(buffer1.shift());  
-        console.log("length after" + i + "shift" + buffer1.length)
-
+        buffer1.shift();  
     }
-    
-    console.log(buffer1.toString());
-    // buffer1.push(0);
-    display.textContent = buffer1.toString();
+        display.textContent = buffer1.toString();
 })
 
 
